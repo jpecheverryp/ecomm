736b179508bb70b653692b71371491409d816522
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-17 20:32:01 -0500

    add chakra ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react';
 import React from 'react';
 
 function App() {
-  return <>Hello</>;
+  return (
+    <Box>
+      <Heading>ecommerce</Heading>
+    </Box>
+  );
 }
 
 export default App;
